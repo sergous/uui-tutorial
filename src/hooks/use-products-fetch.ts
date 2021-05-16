062f84feea59826c2b404b68f9a39831a8a919a0
Sergei Smirnov <sergous@gmail.com>
2021-05-16 19:03:30 -0400

    Adds useProductsFetch hook

---
+++ src/hooks/use-products-fetch.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { Product } from "@epam/uui-docs";
+import { svc } from "../services";
+
+export function useProductsFetch(
+  first = 0,
+  last = 10
+): [Product[] | null, boolean, boolean] {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    svc
+      .getProductItems()
+      .then((res: any) => {
+        setLoading(false);
+        setResponse(res.slice(first, last));
+      })
+      .catch(() => {
+        setHasError(true);
+        setLoading(false);
+      });
+  }, []);
+  return [response, loading, hasError];
+}
